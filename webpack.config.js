ab0c249108388e3db5236ccc2c383ee531933f77
Camilla Cortney <camilla.cortney@gmail.com>
2023-02-27 14:27:48 -0800

    webpack css

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,76 @@
 const webpack = require('webpack'),
-  path = require('path'),
-  CopyWebpackPlugin = require('copy-webpack-plugin'),
-  HtmlWebpackPlugin = require('html-webpack-plugin'),
-  MiniCssExtractPlugin = require('mini-css-extract-plugin');
+    path = require('path'),
+    CopyWebpackPlugin = require('copy-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // for now just production
-process.env.NODE_ENV =  'production'
+process.env.NODE_ENV = 'production'
 
 const options = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: {
-    popup: path.join(__dirname, 'src', 'popup', 'popup.js'),
-    background: path.join(__dirname, 'src', 'background', 'background.js'),
-    firebase_app: path.join(__dirname, 'src', 'firebase', 'firebase-app.js'),
-    firebase_firestore: path.join(__dirname, 'src', 'firebase', 'firebase-firestore.js')
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        use: [
-          {
-            loader: require.resolve('babel-loader'),
-          },
-        ],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.css'],
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/manifest.json',
-          to: path.join(__dirname, 'dist'),
-          force: true,
-        },
-      ],
-    }),
-    // new CopyWebpackPlugin({
-    //   patterns: [
-    //     {
-    //       from: 'src/popup/popup.css',
-    //       to: path.join(__dirname, 'dist'),
-    //       force: true,
-    //     },
-    //   ],
-    // }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'Popup', 'popup.html'),
-      filename: 'popup.html',
-      chunks: ['popup'],
-      cache: false,
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ]
+    mode: process.env.NODE_ENV || 'development',
+    entry: {
+        popup: path.join(__dirname, 'src', 'popup', 'popup.js'),
+        background: path.join(__dirname, 'src', 'background', 'background.js'),
+        firebase_app: path.join(__dirname, 'src', 'firebase', 'firebase-app.js'),
+        firebase_firestore: path.join(__dirname, 'src', 'firebase', 'firebase-firestore.js')
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }, {
+                test: /\.(js|jsx)$/,
+                use: [
+                    {
+                        loader: require.resolve('babel-loader')
+                    },
+                ],
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.css']
+    },
+    plugins: [
+        new CopyWebpackPlugin(
+            {
+                patterns: [
+                    {
+                        from: 'src/manifest.json',
+                        to: path.join(__dirname, 'dist'),
+                        force: true
+                    },
+                ]
+            }
+        ),
+        new CopyWebpackPlugin(
+            {
+                patterns: [
+                    {
+                        from: 'src/popup/popup.css',
+                        to: path.join(__dirname, 'dist'),
+                        force: true
+                    },
+                ]
+            }
+        ),
+        new HtmlWebpackPlugin(
+            {
+                template: path.join(__dirname, 'src', 'Popup', 'popup.html'),
+                filename: 'popup.html',
+                chunks: ['popup'],
+                cache: false
+            }
+        ),
+    ]
 };
 
 module.exports = options;
