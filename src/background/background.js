bcb478e21303f4e64e7c2154c504e2241d21fff5
Camilla Cortney <camilla.cortney@gmail.com>
2023-02-27 14:28:01 -0800

    restrict to amazon product/search pages

--- src/background/background.js
+++ src/background/background.js
@@ -1,85 +1,78 @@
 // I am still getting an import error, but everything is working. Look into this.
-import { initializeApp } from "../firebase/firebase-app.js"
-import {    getFirestore, 
-            collection, 
-            query, 
-            where, 
-            getDocs, 
-            addDoc, 
-            doc 
-        } from "../firebase/firebase-firestore.js"
+import {initializeApp} from "../firebase/firebase-app.js"
+import {
+    getFirestore,
+    collection,
+    query,
+    where,
+    getDocs,
+    addDoc,
+    doc
+} from "../firebase/firebase-firestore.js"
 
 const firebaseConfig = {
     apiKey: 'AIzaSyCUOWLQzjGuHfsd0Ih6MmEiOwcdoLYWyUM',
     authDomain: 'greener-80296.firebaseapp.com',
-    databaseURL:
-      'https://greener-80296-default-rtdb.europe-west1.firebasedatabase.app',
+    databaseURL: 'https://greener-80296-default-rtdb.europe-west1.firebasedatabase.app',
     projectId: 'greener-80296',
     storageBucket: 'greener-80296.appspot.com',
     messagingSenderId: '976797593748',
     appId: '1:976797593748:web:abbdaa9c42e1101af3c481',
-    measurementId: 'G-QTMCMYZHKN',
-  };
-
-// domain = domain.replace('http://', '').replace('https://', '').replace('www.','').split(/[/?#]/)[0];
+    measurementId: 'G-QTMCMYZHKN'
+};
 
 const firebase_app = initializeApp(firebaseConfig);
 const db = getFirestore(firebase_app)
 
+async function getTab(query) {
+    let result = await chrome.tabs.query(query)
+    let domain = getDomain(result[0].url)
+    let productPage = checkUrl(result[0].url)
+    return {tabName: result[0].title, domain: domain, url: result[0].url, productPage: productPage}
+}
+
 function getDomain(url) {
     let domain = url;
-    domain = domain.replace('http://', '')
-        .replace('https://', '')
-        .replace('www.','')
-        .split(/[/?#]/)[0];
+    domain = domain.replace('http://', '').replace('https://', '').replace('www.', '').split(/[/?#]/)[0];
     return domain
 }
 
-async function getTab(query) {
-    let result = await chrome.tabs.query(query)
-    // console.log(result[0])
-    let domain = getDomain(result[0].url)
-    // console.log(domain)
-    return result[0].title
+function checkUrl(url) {
+    const substrings = ["/dp", "/gp/product", "s?k="]
+    const result = substrings.some(str => url.includes(str))
+    return result
 }
 
-async function get_database_elements(db_name){
+async function get_database_elements(db_name) {
     const q = query(collection(db, db_name));
     const querySnapshot = await getDocs(q);
     return querySnapshot
 }
 
-function send_data(snapshot, webTitle) {
-    // we are sending a message back to popup.js with data
-    chrome.runtime.sendMessage( { text: {
-        data: snapshot.docs[0].data(), 
-        webTitle: webTitle[0].title
-    }, command: 'GET' });
+function send_data(snapshot, tabInfo) { // we are sending a message back to popup.js with data
+    chrome.runtime.sendMessage({data: snapshot.docs[0].data(), domain: tabInfo.domain, tabName: tabInfo.tabName, command: 'AMAZON - PRODUCT'});
 }
 
-chrome.runtime.onMessage.addListener( async (msg) => {
-    // this will return the title of the tab, which gives the current amazon product
-    let webTitle = await getTab({active: true, lastFocusedWindow: true})
-    console.log(`WebTitle: ${webTitle}`)
-
-    // I am not sure if we will be sending other commands other than GET from popup.js
-    // If not, we can end up removing this if statement
-    if (msg.command === 'GET') {
-        // This allows me to see in the service worker, what data I am retrieving from Firestore
+chrome.runtime.onMessage.addListener(async (msg) => {
+    let tabInfo = await getTab({active: true, lastFocusedWindow: true})
+    if (tabInfo.domain === "amazon.com" && tabInfo.productPage) {
+        // This allows me to see in the service worker,
+        // what data I am retrieving from Firestore
         // This won't stay long term
         let products = [];
 
-        get_database_elements('products')
-            .then((snapshot) => {
-                send_data(snapshot, webTitle)
-                // Do I need a .then, .catch?
-                snapshot.docs.forEach((doc) => {
-                    products.push(doc.data())
-                })
+        get_database_elements('products').then((snapshot) => {
+            send_data(snapshot, tabInfo)
+            snapshot.docs.forEach((doc) => {
+                products.push(doc.data())
+            })
             console.log(products)
-            })
-            .catch(err => {
+        }).catch(err => {
             console.log(err.message)
-            })
+        })
+    } else if (tabInfo.domain === 'amazon.com') {
+        chrome.runtime.sendMessage({command: 'AMAZON - NOT PRODUCT'})
+    } else {
+        chrome.runtime.sendMessage({command: 'NOT AMAZON'})
     }
 })
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,19 +1,47 @@
+// Retrieve webpage data
+chrome.runtime.sendMessage({command: 'GET'})
 
-chrome.runtime.sendMessage( { text: '?', command: 'GET' })
-
-chrome.runtime.onMessage.addListener( (msg) => {
-    document.body.innerHTML = `
+chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.command === 'AMAZON - PRODUCT') {
+        document.body.innerHTML = `
     <div>
-        Alternative: ${msg.text.data.name}<br>
-        <img src="https:${msg.text.data.img}">
+        Alternative: ${
+            msg.data.name
+        }<br>
+        <img src="https:${
+            msg.data.img
+        }">
         <button class="product-link">Open Product Page</button>
     </div>
     `
-    const link = document.querySelector('.product-link')
-    
-    link.addEventListener('click', function (e) {
-        chrome.tabs.create({
-            url:`${msg.text.data.link}`
-          });
-    })
+        const link = document.querySelector('.product-link')
+
+        link.addEventListener('click', function (e) {
+            chrome.tabs.create({
+                    url: `${
+                    msg.data.link
+                }`
+            });
+        })
+    } else if (msg.command === 'AMAZON - NOT PRODUCT') {
+        document.body.innerHTML = `
+            <div>
+                Click on a product to start finding alternatives!
+            </div>
+        `
+    } else {
+        document.body.innerHTML = `
+    <div>
+        Go to Amazon to start shopping
+    <br>
+        <button class="amazon-link">Open Amazon</button>
+    </div>
+    `
+        const link = document.querySelector('.amazon-link')
+
+        link.addEventListener('click', function (e) {
+            chrome.tabs.create({url: `https://www.amazon.com`});
+        })
+
+    }
 })
