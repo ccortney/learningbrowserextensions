3dfa2aacaaf34fd813b718cbb09c279841306be4
Camilla Cortney <camilla.cortney@gmail.com>
2023-04-04 01:04:50 -0700

    simple popup html/css styling

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,76 +1,126 @@
-// Retrieve webpage data
-function startPopup() {
-    chrome.runtime.sendMessage({command: 'GET'})
+chrome.runtime.sendMessage({command: 'GET'})
 
-    chrome.runtime.onMessage.addListener((msg) => {
-        if (msg.command === 'AMAZON - PRODUCT FOUND') {
-            document.body.innerHTML = `
-        <div>
-            Alternative: ${
-                msg.data.name
-            }<br>
-                <a href="">
-                    <img src="https:${
-                msg.data.img
-            }">
-                </a>
-            <br>
+chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.command === 'AMAZON - PRODUCT FOUND') {
+        let alert = "Alternative Found!"
+        let text = "Try this product instead!"
+
+        document.body.innerHTML = createPopUpHTMLwithProduct(alert, text, msg)
+
+        const image = document.querySelector('.productImage')
+
+        image.addEventListener('click', function (e) {
+            chrome.tabs.create({
+                    url: `${
+                    msg.data.link
+                }`
+            });
+        })
+    } else if (msg.command === 'AMAZON - PRODUCT IS ALTERNATIVE') {
+        let alert = "Excellent Choice!"
+        let text = "This product is sustainable and <br> environmentally-friendly."
+        
+        document.body.innerHTML = createPopUpHTML(alert, text, false)
+
+    } else if (msg.command === 'AMAZON - PRODUCT NOT FOUND') {
+        let alert = "No Alternive Found!"
+        let text = "We are adding more products every day, <br> check again soon."
+        
+        document.body.innerHTML = createPopUpHTML(alert, text, false)
+
+    } else if (msg.command === 'AMAZON - NOT PRODUCT PAGE') {
+        let alert = "No Alternive Found!"
+        let text = "Open a product to start finding sustainable, <br> environmentally-friendly alternatives!"
+
+        document.body.innerHTML = createPopUpHTML(alert, text, false)
+
+    } else if (msg.command === 'NOT AMAZON SITE') {
+
+        let text = "Start finding sustainable, environmentally-friendly alternatives for products on Amazon."
+
+        document.body.innerHTML = createPopUpHTML(null, text, true)
+
+        const link = document.querySelector('.amazon-link')
+
+        link.addEventListener('click', function (e) {
+            chrome.tabs.create({url: `https://www.amazon.com`});
+        })
+    } else {
+        let alert = "Oh snap! Something went wrong!"
+        let text = "Try again later."
+        
+        document.body.innerHTML = createPopUpHTML(alert, text, false)
+    }
+})
+
+function createPopUpHTML(alert, text, button) {
+
+    let headerHTML = `            
+        <div class = "header">
+            <h3> 
+                <img src="assets/greenicon.png" class="icon">
+                Greener Healthier Happier
+            </h3>
         </div>
-        `
-            const image = document.querySelector('img')
+    `
 
-            image.addEventListener('click', function (e) {
-                chrome.tabs.create({
-                        url: `${
-                        msg.data.link
-                    }`
-                });
-            })
-        } else if (msg.command === 'AMAZON - PRODUCT IS ALTERNATIVE') {
-            document.body.innerHTML = `
+    let messageHTML;
+    if (!alert) {
+        messageHTML = `<p>${text}</p>`
+
+    } else {
+        messageHTML = `
+            <p class = "alert">${alert}</p>
+            <p>${text}</p>
+        `
+    }
+
+    let buttonHTML = `
+        <div class="button">
+            <button class="amazon-link" >Open Amazon</button>
+        </div>
+    `
+
+    if (button) {
+        return `
             <div>
-                Great find! No alternative needed.
+                ${headerHTML}
+                <div class = "message">
+                    ${messageHTML}
+                </div>
+                ${buttonHTML}
             </div>
         `
-        } else if (msg.command === 'AMAZON - PRODUCT NOT FOUND') {
-            document.body.innerHTML = `
-                <div>
-                    No alternative found at this time.
-                </div>
-            `
-        } else if (msg.command === 'AMAZON - NOT PRODUCT PAGE') {
-            document.body.innerHTML = `
-                <div>
-                    Click on a product to start finding alternatives!
-                </div>
-            `
-        } else if (msg.command === 'NOT AMAZON SITE') {
-            document.body.innerHTML = `
+    } else {
+        return `
             <div>
-                Go to Amazon to start shopping!
-            <br>
-                <button class="amazon-link" >Open Amazon</button>
+                ${headerHTML}
+                <div class = "message">
+                    ${messageHTML}
+                </div>
             </div>
-            `
-            const link = document.querySelector('.amazon-link')
-
-            link.addEventListener('click', function (e) {
-                chrome.tabs.create({url: `https://www.amazon.com`});
-            })
-        } else {
-            document.body.innerHTML = `
-            404 ERROR
-            `
-        }
-    })
+    `   
+    }
 }
 
-startPopup()
-
-// chrome.webNavigation.onCommitted.addListener(details => {
-//     alert('hi')
-// })
-
-module.exports = {
-    startPopup
+function createPopUpHTMLwithProduct(alert, text, msg) {
+    let html = createPopUpHTML(alert, text, false)
+    let productHTML = `
+        <div class="product">
+            <a href="">
+                <img 
+                    src="https:${msg.data.img}" 
+                    alt="${msg.data.name}"
+                    class="productImage"
+                >
+            </a>
+        </div>
+    `
+    return html += productHTML
 }
+
+
+
+// module.exports = {
+//     startPopup
+// }
